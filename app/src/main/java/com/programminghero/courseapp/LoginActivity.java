367cc5a9a8c54237a875c35272b33dd3df90798c
Md Rashad Tanjim <arnob.tanjim@gmail.com>
2020-12-30 16:09:32 +0600

    login invalid dialog UI added

--- app/src/main/java/com/programminghero/courseapp/LoginActivity.java
+++ app/src/main/java/com/programminghero/courseapp/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.programminghero.courseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        (findViewById(R.id.signIn)).setOnClickListener(v -> showCustomDialog());
+
+        showCustomDialog();
+    }
+
+    private void showCustomDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_warning);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+
+        (dialog.findViewById(R.id.button_close)).setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 }
